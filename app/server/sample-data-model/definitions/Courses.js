8f629d36f336ad34f6fda334539fc577e1bac84c
Aljon Preza <aljonpreza@yahoo.com>
2016-01-16 18:41:28 -1000

    Defined 4 Courses

--- app/server/sample-data-model/definitions/Courses.js
+++ app/server/sample-data-model/definitions/Courses.js
@@ -20,6 +20,35 @@ defineCourses = function() {
       number: "ICS 311",
       description: "Design and correctness of algorithms, including divide-and-conquer, greedy and dynamic programming methods.",
       tags: ["software-engineering", "silicon-valley"]
+    },
+      /* Define 4 additional courses */
+    {
+      name: "Discrete Math for CS I",
+      slug: "ics141",
+      number: "ICS 141",
+      description: "Includes logic, sets, functions, matrices, algorithmic concepts, mathematical reasoning, recursion, counting techniques, and probability theory.",
+      tags: []
+    },
+    {
+      name: "Data Visualization",
+      slug: "ics484",
+      number: "ICS 484",
+      description: "Introduction to data visualization through practical techniques for turning data into images to produce insight.",
+      tags: []
+    },
+    {
+      name: "Software Engineering I",
+      slug: "ics314",
+      number: "ICS 314",
+      description: "Problem analysis and design, team-oriented development, quality assurance, configuration management, project planning.",
+      tags: []
+    },
+    {
+      name: "AI for Games",
+      slug: "ics462",
+      number: "ICS 462",
+      description: "Techniques to stimulate intelligence in video games: movement, pathfinding with A* search, decision/behavior trees, state machines, machine learning, tactics.",
+      tags: []
     }
   ];
 
